14d249fdadca2a885a546f2d02b49e2d257c03c3
Neksyq <neksandrianov@gmail.com>
2024-11-02 00:32:23 +0100

    Tween Animation for the XP bar and new levels

--- lib/widgets/xpBar/xp_bar.dart
+++ lib/widgets/xpBar/xp_bar.dart
@@ -33,9 +33,16 @@ class XPBarState extends State<XPBar> {
   }
 
   int calculateLevel(double xp) {
-    if (xp < 1000.0) return 1;
-    if (xp < 3000.0) return 2;
-    if (xp < 6000.0) return 3;
+    if (xp < 100.0) return 0;
+    if (xp < 500.0) return 1;
+    if (xp < 1000.0) return 2;
+    if (xp < 1750.0) return 3;
+    if (xp < 2500.0) return 4;
+    if (xp < 3500.0) return 5;
+    if (xp < 4750.0) return 6;
+    if (xp < 6250.0) return 7;
+    if (xp < 8250.0) return 8;
+    if (xp < 10000.0) return 9;
     return 10;
   }
 
@@ -49,14 +56,21 @@ class XPBarState extends State<XPBar> {
           style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 8),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: LinearProgressIndicator(
-            value: xp,
-            minHeight: 10,
-            backgroundColor: Colors.grey[300], // Background color of the bar
-            color: Colors.green, // Progress color
-          ),
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(
+              begin: 0, end: xp / 10000), // Adjust 10000 as max XP for level up
+          duration: const Duration(milliseconds: 500),
+          builder: (context, value, child) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: LinearProgressIndicator(
+                value: value,
+                minHeight: 10,
+                backgroundColor: Colors.grey[300],
+                color: Colors.green,
+              ),
+            );
+          },
         ),
         const SizedBox(height: 8),
       ]),
